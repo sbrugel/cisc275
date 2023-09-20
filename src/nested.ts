692bc3a1143b2a8475126b0931eaa54d180b2f9e
Simon <sbrugel@udel.edu>
2023-09-20 11:53:06 -0400

    getNames

--- src/nested.ts
+++ src/nested.ts
@@ -47,7 +47,7 @@ export function removeQuestion(questions: Question[], id: number): Question[] {
  * questions, as an array.
  */
 export function getNames(questions: Question[]): string[] {
-    return [];
+    return questions.map((q) => q.name);
 }
 
 /***
