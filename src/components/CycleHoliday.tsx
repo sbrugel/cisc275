0223284cb645b258e9b86c704988d4d98df7cce7
Simon <sbrugel@udel.edu>
2023-09-22 15:26:51 -0400

    CycleHolidays

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,39 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+export function getNextByDate(holiday: string): string {
+    const order = ["🥚", "🚂", "🦃", "🎄", "🎂"];
+    if (order.indexOf(holiday) === 4) return order[0];
+    else {
+        return order[order.indexOf(holiday) + 1];
+    }
+}
+
+export function getNextByAlphabet(holiday: string): string {
+    const order = ["🎂", "🎄", "🥚", "🚂", "🦃"];
+    if (order.indexOf(holiday) === 4) return order[0];
+    else {
+        return order[order.indexOf(holiday) + 1];
+    }
+}
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [currentHoliday, setCurrentHoliday] = useState<string>("🎂");
+    return (
+        <>
+            <p>Holiday: {currentHoliday}</p>
+            <Button
+                onClick={() =>
+                    setCurrentHoliday(getNextByAlphabet(currentHoliday))
+                }
+            >
+                Next in Alphabet
+            </Button>
+            <Button
+                onClick={() => setCurrentHoliday(getNextByDate(currentHoliday))}
+            >
+                Next in Year
+            </Button>
+        </>
+    );
 }
